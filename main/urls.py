5138179b6cf4e0b5efd8884658767706545198a9
vlad-che <vlad1010che@gmail.com>
2023-11-02 17:59:39 +0600

    lesson_57 end

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import index, other_page, BbLoginView
+from .views import (index, other_page, BbLoginView, profile, BbLogoutView,
+                    ChangeUserInfoView, BbPasswordChangeView)
 
 app_name = 'main'
 
@@ -8,4 +9,8 @@ urlpatterns = [
     path('<str:page>', other_page, name='other'),
     path('', index, name='index'),
     path('accounts/login/', BbLoginView.as_view(), name='login'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', BbLogoutView.as_view(), name='logout'),
+    path('accounts/password/change/', BbPasswordChangeView.as_view(), name='password_change'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template.loader import get_template
 from django.template.exceptions import TemplateDoesNotExist
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView
+
+from .models import AdvUser
+from .forms import ChangeUserInfoForm
 
 
 def index(request):
@@ -19,9 +26,27 @@ def other_page(request, page):
 
 
 class BbLoginView(LoginView):
-    template_name = 'main/login.html'
+    template_name = 'main/accounts/login.html'
 
 
 @login_required
 def profile(request):
-    return render(request, 'main/profile.html')
+    return render(request, 'main/accounts/profile.html')
+
+
+class BbLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/accounts/logout.html'
+
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = AdvUser
+    template_name = 'main/accounts/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Данные пользователя изменены'
+
+
+class BbPasswordChangeView(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
+    template_name = 'main/accounts/password_change.html'
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Пароль пользователя изменён'
