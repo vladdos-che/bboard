73eff1d6c53fc07cd276a8a92232902a2286bf53
vlad-che <vlad1010che@gmail.com>
2023-11-02 16:27:13 +0600

    lesson_57 mid

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import index, other_page, BbLoginView
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', index, name='index')
+    path('<str:page>', other_page, name='other'),
+    path('', index, name='index'),
+    path('accounts/login/', BbLoginView.as_view(), name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,27 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
+from django.template.exceptions import TemplateDoesNotExist
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def other_page(request, page):
+    try:
+        template = get_template('main/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
+
+
+class BbLoginView(LoginView):
+    template_name = 'main/login.html'
+
+
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
